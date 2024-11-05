ee3e96f8a5d1c49cbc19d0cbc5b98bb996e174f5
aashritha-Kore <160595295+aashritha-Kore@users.noreply.github.com>
2024-11-05 17:39:59 +0530

    removing from root level (#27)

--- customConnectorService/routes/config.route.js
+++
@@ -1,8 +0,0 @@
-const route = require('express').Router()
-const { get_config_controller } = require("../controller/config.controller")
-const { checkAuthorized } = require("../middleware/authorization")
-
-//route  to create ticket
-route.get('/searchAssistant/getConfig', checkAuthorized, get_config_controller)
-
-module.exports = route
--- customConnectorService/routes/content.route.js
+++
@@ -1,8 +0,0 @@
-const route = require('express').Router()
-const { get_content_controller } = require("../controller/content.controller")
-const { checkAuthorized } = require("../middleware/authorization")
-
-//route  to get content 
-route.get('/searchAssistant/getContent', checkAuthorized, get_content_controller)
-
-module.exports = route
